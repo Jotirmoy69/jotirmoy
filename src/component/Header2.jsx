d0613ee07a900e2963ac3fc2feac4f20fd5fab47
Jotirmoy <jotirmoyff1976@gmail.com>
2025-02-20 00:19:26 +0600

    first commit

---
+++ src/component/Header2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Header2 = () => {
+  return (
+    <div className='fixed  top-0 w-full p-16 flex justify-between z-999'>
+      <img src="" alt="" />
+    <Link to='/' className=" cursor-pointer header md:text-xl text-sm border-4 border-gray-200 font-bold transition-all duration-300 hover:bg-white hover:text-black font-[anzo2] px-4 py-2 rounded-full bg-black  text-white">
+          Close
+    </Link>
+    </div>
+  )
+}
+
+export default Header2
