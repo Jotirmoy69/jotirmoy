76b98121ddfa11aec4faa56356d95df0632a04f2
Jotirmoy <jotirmoyff1976@gmail.com>
2025-09-11 20:11:59 +0600

    F

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: 'https://jotirmoy.vercel.app', // your real URL
   generateRobotsTxt: true,                // automatically generate robots.txt
-  generateIndexSitemap: false,
+  // generateIndexSitemap: false,
   exclude: ['/api/*', '/_next/*'],       // exclude API routes and Next.js internals
   sitemapSize: 5000,
   robotsTxtOptions: {
