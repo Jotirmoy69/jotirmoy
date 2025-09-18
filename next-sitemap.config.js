b0a4fed85cdfe2300d402a829710752b7bbb57c3
Jotirmoy <jotirmoyff1976@gmail.com>
2025-09-19 01:12:15 +0600

    wrong

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://jotirmoy.vercel.app',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
-};
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: 'https://jotirmoy.vercel.app', // your deployed URL
+//   generateRobotsTxt: true,
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: '*',
+//         allow: '/',
+//       },
+//     ],
+//     additionalSitemaps: [
+//       'https://jotirmoy.vercel.app/sitemap.xml',
+//     ],
+//   },
+// };
